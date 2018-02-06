61269eb349c765f904cf80b9a16d487abea30e15
rommel-casa\Rommel <rommel.alan.sanchez@gmail.com>
2018-02-05 23:07:30 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+    context: __dirname + "/src",
+    devtool: debug ? "inline-sourcemap" : null,
+    entry: "./js/client.js",
+    module: {
+
+        loaders: [
+            {
+                text: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0'],
+                    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+                }
+            }
+        ]
+    },
+    output: {
+        path: __dirname + "/src/",
+        filename: "client.min.js"
+    },
+    plugins: debug ? [] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourceMap: false })
+    ]
+};
